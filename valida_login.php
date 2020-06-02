9a79ac15bd3d7a4b9edb98a8574e658e325e1403
kanda <rodrigokanda2016@gmail.com>
2020-06-02 13:48:29 -0400

    first commit

---
+++ valida_login.php
@@ -0,0 +1,38 @@
+<?php
+
+session_start();
+echo $_SESSION['autenticado'];
+
+
+//VARIAVEL QUE VERIFICA SE A AUTENTICAÇÃO FOI REALIZADA
+$usuario_autenticado = false;
+
+//USUARIOS DO SISTEMA
+$usuarios_app = array(
+    array('email' => 'adm@teste.com', 'senha' => 'abcd'),
+    array('email' => 'user@teste.com', 'senha' => 'abcd')
+);
+
+foreach($usuarios_app as $user){
+   
+    if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
+        $usuario_autenticado = true;
+    }
+}
+
+    if($usuario_autenticado){
+        echo 'Usuário autenticado.';
+        $_SESSION['autenticado']='SIM';
+        header('Location: home.php');
+
+    }else{
+        $_SESSION['autenticado']='NÃo';
+
+        header('Location: index.php?login=erro');
+      
+    }
+    
+
+
+
+?>
